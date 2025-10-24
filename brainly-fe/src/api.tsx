c4b0d9feaf4eba8211b31807b09b4d86c2356f73
dufferdeepu <deepeshbind@gmail.com>
2025-10-24 20:38:58 +0530

    connected backend

--- brainly-fe/src/api.tsx
+++ brainly-fe/src/api.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Base URL for all API requests
-const API_URL = 'http://localhost:3000/api/v1';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Simple type definitions
 interface Content {
