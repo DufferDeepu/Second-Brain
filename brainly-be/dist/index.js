8c656b40fdf99fd1626c265febf72f86de0210d2
dufferdeepu <deepeshbind@gmail.com>
2025-10-24 19:58:23 +0530

    fix

--- brainly-be/dist/index.js
+++ brainly-be/dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -26,14 +17,14 @@ app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 const PORT = process.env.PORT || 3000;
 // Sign up endpoint
-app.post('/api/v1/signup', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/api/v1/signup', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     try {
         // Hash password for security
-        const hashedPassword = yield bcrypt_1.default.hash(password, 10);
+        const hashedPassword = await bcrypt_1.default.hash(password, 10);
         // Create new user
-        yield db_1.UserModel.create({
+        await db_1.UserModel.create({
             username,
             password: hashedPassword
         });
@@ -46,18 +37,18 @@ app.post('/api/v1/signup', (req, res) => __awaiter(void 0, void 0, void 0, funct
             message: "User already signed up"
         });
     }
-}));
+});
 // Sign in endpoint
-app.post('/api/v1/signin', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/api/v1/signin', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     // Find user by username
-    const existingUser = yield db_1.UserModel.findOne({
+    const existingUser = await db_1.UserModel.findOne({
         username
     });
     if (existingUser && existingUser.password) {
         // Check if password is correct
-        const isPasswordValid = yield bcrypt_1.default.compare(password, existingUser.password);
+        const isPasswordValid = await bcrypt_1.default.compare(password, existingUser.password);
         if (isPasswordValid) {
             // Create JWT token
             const token = jsonwebtoken_1.default.sign({
@@ -78,15 +69,15 @@ app.post('/api/v1/signin', (req, res) => __awaiter(void 0, void 0, void 0, funct
             message: "Incorrect Credentials"
         });
     }
-}));
+});
 // Add content endpoint
-app.post('/api/v1/content', middleware_1.userMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/api/v1/content', middleware_1.userMiddleware, async (req, res) => {
     console.log("Received request body:", req.body);
     console.log("User ID from token:", req.userId);
     const { link, type, title } = req.body;
     try {
         // Create new content
-        yield db_1.ContentModel.create({
+        await db_1.ContentModel.create({
             link,
             type,
             title,
@@ -102,36 +93,36 @@ app.post('/api/v1/content', middleware_1.userMiddleware, (req, res) => __awaiter
             message: "Error adding content"
         });
     }
-}));
+});
 // Get user content endpoint
-app.get('/api/v1/content', middleware_1.userMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/api/v1/content', middleware_1.userMiddleware, async (req, res) => {
     const userId = req.userId;
     // Get all content for this user
-    const content = yield db_1.ContentModel.find({
+    const content = await db_1.ContentModel.find({
         userId
     }).populate("userId", "username");
     res.json({
         content
     });
-}));
+});
 // Delete content endpoint
-app.post('/api/v1/delete', middleware_1.userMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/api/v1/delete', middleware_1.userMiddleware, async (req, res) => {
     const contentId = req.body.contentId;
     // Delete content for this user
-    yield db_1.ContentModel.deleteMany({
+    await db_1.ContentModel.deleteMany({
         _id: contentId,
         userId: req.userId
     });
     res.json({
         message: "Content deleted"
     });
-}));
+});
 // Share brain endpoint
-app.post('/api/v1/brain/share', middleware_1.userMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/api/v1/brain/share', middleware_1.userMiddleware, async (req, res) => {
     const share = req.body.share;
     if (share) {
         // Check if user already has a share link
-        const existingLink = yield db_1.LinkModel.findOne({
+        const existingLink = await db_1.LinkModel.findOne({
             userId: req.userId
         });
         if (existingLink) {
@@ -143,7 +134,7 @@ app.post('/api/v1/brain/share', middleware_1.userMiddleware, (req, res) => __awa
         // Generate new hash
         const hash = (0, utils_1.random)(10);
         // Create new link
-        yield db_1.LinkModel.create({
+        await db_1.LinkModel.create({
             userId: req.userId,
             hash: hash
         });
@@ -153,19 +144,19 @@ app.post('/api/v1/brain/share', middleware_1.userMiddleware, (req, res) => __awa
     }
     else {
         // Remove share link
-        yield db_1.LinkModel.deleteOne({
+        await db_1.LinkModel.deleteOne({
             userId: req.userId
         });
         res.json({
             message: "Removed link"
         });
     }
-}));
+});
 // Get shared brain endpoint
-app.get("/api/v1/brain/:shareLink", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
     const hash = req.params.shareLink;
     // Find link by hash
-    const link = yield db_1.LinkModel.findOne({
+    const link = await db_1.LinkModel.findOne({
         hash
     });
     if (!link) {
@@ -175,12 +166,12 @@ app.get("/api/v1/brain/:shareLink", (req, res) => __awaiter(void 0, void 0, void
         return;
     }
     // Get all content for shared user
-    const content = yield db_1.ContentModel.find({
+    const content = await db_1.ContentModel.find({
         userId: link.userId
     });
     console.log(link);
     // Get user info
-    const user = yield db_1.UserModel.findOne({
+    const user = await db_1.UserModel.findOne({
         _id: link.userId
     });
     if (!user) {
@@ -193,7 +184,7 @@ app.get("/api/v1/brain/:shareLink", (req, res) => __awaiter(void 0, void 0, void
         username: user.username,
         content: content
     });
-}));
+});
 // Start server
 app.listen(PORT, () => {
     console.log("Server running on port 3000");
--- brainly-be/src/config.ts
+++ brainly-be/src/config.ts
@@ -1,13 +1,10 @@
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// Load environment variables from .env file
 dotenv.config();
 
-// Get environment variables and export them
 export const JWT_SECRET = process.env.JWT_SECRET || "";
 export const MONGO_URI = process.env.MONGO_URI || "";
 
-// Check if required environment variables are available
 if (!JWT_SECRET || !MONGO_URI) {
   throw new Error("Missing required environment variables!");
 }
